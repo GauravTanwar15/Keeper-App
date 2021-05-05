222fed843ce21e2575b019f4cb259d34aed857c2
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-05-05 20:33:29 +0530

    Deploy Website

---
+++ build/precache-manifest.86a40c463b62b3d825c9d03548ce66a5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c67ae11387e9dcbbd0c1cf716cb573",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "b37655fa39a9270e8306",
+    "url": "/Keeper-App/static/js/2.238b1ca2.chunk.js"
+  },
+  {
+    "revision": "51150f8532e6399badad",
+    "url": "/Keeper-App/static/js/main.d8ac152a.chunk.js"
+  },
+  {
+    "revision": "450f8c8ae8899abc2755",
+    "url": "/Keeper-App/static/js/runtime-main.d50d7308.js"
+  }
+]);
